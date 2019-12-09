fd4601add988e2111031a0b8c8841f1d4b9d9b61
devolusegun <54096690+devolusegun@users.noreply.github.com>
2019-12-09 18:57:58 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,26 +1,58 @@
 <?php
-
-
 /** 
  * Main file that is responsible to successfully sign-on.
  */
 
-	chdir(dirname(__FILE__));
+chdir(dirname(__FILE__));	
+include('./config.php');
+
+$id;
+$locate;
+$is_logged = FALSE;
+$info;
+$keepmelog = 0;
+$sess = array();
+
+session_start();
+$db1 = myconnOpen();
+
+function login($uname, $pwd)
+{
+	if(empty($uname) ) return FALSE;
+	$login = $db1->escape($uname);
+	$pass = $db1->escape($pwd);
 	
-	require_once('./config.php');
+	// First check if there is a user with email or username
+	$exist = $db1->query("SELECT iduser, password FROM users WHERE (email='".$login."' OR username='".$login."') AND active=1 LIMIT 1");
 	
-	session_start();
+	//Verify password match
+	if(!$obj = $db1->fetch_object()) return FALSE;
+	$password = $obj->password;
+	$cleartext = hash('sha512', $pass);
+	if($password != $cleartext) return FALSE;
 	
-	$db1 = new mysql($C->DB_HOST, $C->DB_USER, $C->DB_PASS, $C->DB_NAME);
-	$db2 = &$db1;
-		
-	$network = new network();
-	$network->load();
+	$info = $locate->get_user_by_id($obj->iduser, TRUE);
+	if(!$info) return FALSE;
 	
-	$user = new user();
-	$user->load();
+	$is_logged = TRUE;
+	$keepmelog = 1;
+	$sess['IS_LOGGED'] = TRUE;
+	$sess['LOGGED_USER'] = & $info;
+	$id = $info->iduser;
 	
-	$page = new page();
-	$page->load();
+	//Get user IP detail and update related user table in DB
+	$ip = $db1->escape(ip2long($_SERVER['REMOTE_ADDR']) );
+	$db1->query('UPDATE users SET previousaccess=lastaccess, ippreviousaccess=iplastaccess, lastaccess="'.time().'", iplastaccess="'.$ip.'", lastclick="'.time().'" WHERE iduser='.$id.' LIMIT 1');
+	
+	// Session expiry setting
+	$sess['total_pageviews'] = 0;
+	if($keepmelog == 1){
+		$tmp = $id.'_'.$pass.'_'.md5($username.'~~'.$password.'~~'.$_SERVER['HTTP_USER_AGENT']);
+		setcookie('keepmelog', $tmp, time()+60*24*60*60, '/');
+	}
+	return redirect('./dashboard');
+	
+	//OR header("Location: dashboard.php");
+}
 
 ?>
